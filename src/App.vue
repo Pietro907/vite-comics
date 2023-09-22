c470a5b1ba96b7d9ffa0d9412a42cfe5ff8700ac
Pietro907 <pietrop.puri@gmail.com>
2023-09-22 15:21:44 +0200

    add initial page and add app.scss

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<script>
+//import HelloWorld from './components/HelloWorld.vue'
+import AppHeader from './components/AppHeader.vue'
+import app from './assets/css/app.scss'
+
+
+export default {
+  // questo é il vs options object
+  data(){
+    return{
+      
+
+
+    }
+
+  },
+  components: {
+    AppHeader,
+  }
+}
+
+</script>
+
+<template>
+
+  <AppHeader></AppHeader>
+
+</template>
+
+<style>
+
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,25 @@
+<script>
+
+export default {
+  // questo é il vs options object
+  data(){
+    return{
+    
+    }
+
+  },
+ 
+}
+
+</script>
+
+
+<template>
+
+</template>
+
+
+<style>
+
+</style>
+
