15d29dba9a79175f0ea749c77c57711bd1dc4c2c
Pietro907 <pietrop.puri@gmail.com>
2023-09-25 13:20:19 +0200

    add img with cards(props) into AppMain

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -5,6 +5,7 @@ import cards from '../card.js'
 export default {
   // questo é il vs options object
   name: 'AppMain',
+
   
   data() {
     return {
@@ -26,11 +27,14 @@ export default {
 
       <div class="row">
 
-        <div v-for="item in cards" class="col-6">
-          <img src="../assets/img/buy-comics-merchandise.png" alt="">
-          <h2>{{item.price}}</h2>
-          <h2>series</h2>
-          <h2>type</h2>
+        <div v-for="(card, i) in cards.cards" class="col-2">
+          <!-- <h2>{{card.price}}</h2> -->
+          <div class="card">
+            <img :src="card.thumb" alt="">
+
+            <h6>{{card.series}}</h6>
+          </div>
+          <!-- <h2>{{card.type}}</h2> -->
         </div>
       </div>
 
@@ -56,15 +60,23 @@ main {
   width: 70%;
   margin: 0 auto;
 }
-
-.col-6 {
-  color: #ffffff;
+.row {
   display: flex;
-  flex-direction: column;
-  align-items: center;
-  padding: 0.5rem 0;
+  flex-wrap: wrap;
+  padding: 1rem 0 0;
 }
-
+.col-2 {
+  color: #ffffff;
+  padding: 0.5rem 0.5rem;
+  margin: 0 5px;
+}
+.card> img {
+  width: 110px;
+  aspect-ratio: 1 / 1;
+}
+.card {
+  width: 100px;
+}
 .col-12>h2 {
   color: #ffffff;
 }
